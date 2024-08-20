4238484b93a32dac9dbe712f8367c1342ca03226
Carla Silva <edicarlasillva@gmail.com>
2024-08-19 22:14:07 -0300

    adicionado switch

--- back-end-I/aula-03/scripts.js
+++ back-end-I/aula-03/scripts.js
@@ -106,7 +106,66 @@
 
 // document.write('Hello')
 
-document.write(`
-  <mark>Hello, </mark></br>
-  <strong>Carla</strong>
-`)
+// document.write(`
+//   <mark>Hello, </mark></br>
+//   <strong>Carla</strong>
+// `)
+
+// let diaDaSemana = 7
+// let nomeDoDia
+
+// switch (diaDaSemana) {
+//   case 1:
+//     nomeDoDia = 'Domingo';
+//     break;
+//   case 2:
+//     nomeDoDia = 'Segunda';
+//     break;
+//   case 3: 
+//     nomeDoDia = 'Terça';
+//     break;
+//   case 4: 
+//     nomeDoDia = 'Quarta';
+//     break;
+//   case 5: 
+//     nomeDoDia = 'Quinta';
+//     break;
+//   case 6: 
+//     nomeDoDia = 'Sexta';
+//     break;
+//   case 7: 
+//     nomeDoDia = 'Sábado';
+//     break;
+//   default:
+//     nomeDoDia = 'Dia inválido'
+// }
+
+// console.log(nomeDoDia)
+
+// const numero = 4
+
+// switch (true) {
+//   case numero > 5: 
+//     console.log('Numero maior que 5.')
+//     break
+//   case numero === 5:
+//     console.log('Número é igual a 5')
+//     break
+//   default:
+//     console.log('O número é menor que 5.')
+// }
+
+// condicao ? valor1 : valor2
+// const idade = 16
+
+// const status = (idade >= 18) ? 'Adulto' : 'Menor'
+
+// console.log(status)
+
+// const resultado = condicao1 ? valor1 : condicao2 ? valor2 : valor3
+
+// const media = 9.65895
+// console.log(media.toFixed(2))
+
+// const anoAtual = new Date().getFullYear()
+// console.log(anoAtual)
