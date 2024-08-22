eb5617ab5c903eae5102de9263c1a48d2aeaa73a
Carla Silva <edicarlasillva@gmail.com>
2024-08-21 21:43:51 -0300

    adicionado for

--- back-end-I/aula-04/scripts.js
+++ back-end-I/aula-04/scripts.js
@@ -31,10 +31,80 @@
 //   }
 // }
 
-let numero
+// let numero
+
+// do {
+//   numero = parseInt(prompt('Digite um número maior que 10.'))
+// } while(numero <= 10)
+
+// alert(`Você digitou um número válido: ${numero}`)
+
+// let numero = parseInt(prompt('Digite um número inteiro e positivo.'))
+// let contador = 1
+// let resultado = ''
+
+// while (contador <= numero) {
+//   resultado += contador + ' '
+//   contador++
+// }
+
+// alert(`${resultado} Acabou!`)
+
+// let numero = parseInt(prompt('Digite um número inteiro e positivo.'))
+// let contador = 1
+// let resultado = ''
+
+// do {
+//   resultado += contador + ' '
+//   contador++
+// } while(contador <= numero)
+
+// alert(`${resultado} Acabou!`)
+
+// let numero
+// let resultado = ''
+// let contador = 1
+
+// do {
+//   numero = parseInt(prompt('Digite um número inteiro e positivo.'))
+// } while (isNaN(numero) || numero <= 0)
+
+// while (contador <= numero) {
+//   resultado += contador + ' '
+//   contador++
+// }
+
+// alert(`${resultado} Acabou!`)
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i)
+// }
+
+// let numero = parseInt(prompt('Digite um número'))
+
+// for (let i = 1; i <= 10; i++) {
+//   let resultado = numero * i
+//   console.log(`${numero} x ${i} = ${resultado}`)
+// }
+
+// let numero = parseInt(prompt('Digite um número'))
+
+// let contador = 1
+
+// while (contador <= 10) {
+//   let resultado = numero * contador
+//   console.log(`${numero} x ${contador} = ${resultado}`)
+
+//   contador++
+// }
+
+let numero = parseInt(prompt('Digite um número'))
+
+let contador = 1
 
 do {
-  numero = parseInt(prompt('Digite um número maior que 10.'))
-} while(numero <= 10)
+  let resultado = numero * contador
+  console.log(`${numero} x ${contador} = ${resultado}`)
 
-alert(`Você digitou um número válido: ${numero}`)
+  contador++
+} while (contador <= 10)
