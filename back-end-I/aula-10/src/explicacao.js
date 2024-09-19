d31a631ea3e9dc1f329673bcb335870dd4ea7c5b
Carla Silva <edicarlasillva@gmail.com>
2024-09-18 22:38:13 -0300

    adicionado node api

---
+++ back-end-I/aula-10/src/explicacao.js
@@ -0,0 +1,30 @@
+console.log('Hello, JS no back-end!')
+
+//Métodos HTTP
+// GET - Pegar informações da API
+// POST - Adicionar informações na API
+// PUT - Atualizar informações na API
+// DELETE - Remover informações na API
+
+// C (POST)
+// R (GET)
+// U (PUT)
+// D (DELETE)
+
+// JSON
+// {
+//   "tarefas": [
+//     {
+//       "id": 1,
+//       "descricao": 'Ir ao mercado.',
+//       "concluida": false
+//     },
+//     {
+//       "id": 2,
+//       "descricao": 'Estudar node.',
+//       "concluida": true
+//     }
+//   ]
+// }
+
+// https://api.github.com/users/edicarlasillva/repos
