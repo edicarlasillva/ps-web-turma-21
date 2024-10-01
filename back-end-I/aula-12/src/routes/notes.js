a8773a16a1df596a5fbea4cefd4c0acafa1888e4
Carla Silva <edicarlasillva@gmail.com>
2024-09-30 22:22:05 -0300

    adicionado rota de criar notes

--- back-end-I/aula-12/src/routes/notes.js
+++ back-end-I/aula-12/src/routes/notes.js
@@ -1,4 +1,5 @@
-import express from 'express'
+import express, { response } from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
 import { users } from './users'
 
@@ -6,4 +7,31 @@ const router = express.Router()
 
 const notes = []
 
+router.post('/', (request, response) => {
+  const { title, description, userId } = request.body
+
+  const user = users.find(user => user.id === userId)
+
+  if (!user) {
+    return response.status(404).json({
+      message: 'Usuário não encontado.'
+    })
+  }
+
+  const newNote = {
+    id: uuidv4(),
+    title,
+    description,
+    userId
+  }
+
+  notes.push(newNote)
+
+  response.status(201).json({
+    message: 'Recado cadastrado com sucesso.',
+    note: newNote
+  })
+})
+
 export default router
+
